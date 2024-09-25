1a934dab574410ee098b19c8a53dcee6d6d6f6ae
Paula Osella <paulaosella19@gmail.com>
2024-09-25 20:17:55 -0300

    se agrega la query de firebase para el filtrado de los productos y se agregan mas productos a la tienda

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,21 +1,25 @@
 import { useParams } from 'react-router-dom';
 import ItemList from './ItemList';
 import './ItemListContainer.css'; 
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
 import { useEffect, useState } from 'react';
 import Spinner from 'react-bootstrap/Spinner'; 
 
 function ItemListContainer() { 
     const [items, setItems] = useState([]);
     const [loading, setLoading] = useState(true); 
-    const { categoryId } = useParams();
+    const { categoryId } = useParams(); 
     
     useEffect(() => {
         setLoading(true); 
         const db = getFirestore();
         const itemCollection = collection(db, "items");
 
-        getDocs(itemCollection)
+        const itemQuery = categoryId
+            ? query(itemCollection, where("category", "==", categoryId)) 
+            : itemCollection;
+
+        getDocs(itemQuery)
             .then((snapshot) => {
                 setItems(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
             })
@@ -26,7 +30,7 @@ function ItemListContainer() {
 
     return (
         <div>
-            <h1 className='Titulo'>Productos</h1>
+
             <div className='Contenedor'>
                 {loading ? (
                     <div className="Spinner">
@@ -37,7 +41,8 @@ function ItemListContainer() {
                 ) : (
                     <ItemList items={items} />
                 )}
-            </div>   
+            </div>  
+            <h1 className='Titulo'>Pagando en efectivo tenes 15% de descuento</h1> 
         </div>
     );
 }

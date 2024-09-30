35e97aabecaf36d384a46ce8a350ed7d8a85df1b
Paula Osella <paulaosella19@gmail.com>
2024-09-30 10:13:00 -0300

    se las etiquetas en nav y modificaciones de variables y funciones que no se han utilizado

--- src/components/ItemDetailContainer/AddItemButton .jsx
+++ src/components/ItemDetailContainer/AddItemButton .jsx
@@ -1,9 +1,6 @@
 import "../ItemDetailContainer/AddItemButton.css";
 
 const AddItemButton = ({ onAdd, quantity }) => {
-    const handleClick = () => {
-        onAdd(quantity);
-    };
 
     return (
         <button onClick={()=>onAdd(quantity)} className='add-item-button'>Agregar al carrito</button>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,13 +4,14 @@ import CartContext from "../Context/CartContext";
 import { CartWidget } from "./components/CartWidget";
 import "./NavBar.css"
 import { Link } from "react-router-dom";
+
+
 export const NavBar = () => {
     const { cart } = useContext(CartContext)
 
-    return <>
+    return (
         <nav className="navbar navbar-expand-lg  fixed-top">
             <div className="container-fluid">
-
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -18,27 +19,29 @@ export const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-nav d-flex flex-row">
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page"><Link to="/">Home</Link></a>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page"><Link to="category/deco">Deco</Link></a>
-                        </li>
-                        <a className="navbar-brand"><Link to="/">Ceramic Lovers Club</Link></a>
-                        <li className="nav-item">
-                            <a className="nav-link"><Link to="/category/kitchen">Kitchen</Link></a>
+                            <Link className="nav-link" to="/category/deco">Deco</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link"><Link to="/category/room">Room</Link></a>
+                            <Link className="navbar-brand" to="/">Ceramic Lovers Club</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link"><Link to="cart"><CartWidget />
+                            <Link className="nav-link" to="/category/kitchen">Kitchen</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/category/room">Room</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                <CartWidget />
                                 <span>{cart.length}</span>
-                            </Link></a>
+                            </Link>
                         </li>
                     </ul>
-
                 </div>
             </div>
         </nav>
-    </>
+    )
 }
